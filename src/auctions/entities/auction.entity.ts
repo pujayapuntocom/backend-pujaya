0c3bc4dc50c33792fbc61cf8e003ddac900515f5
omarenriquecs <omarenriquecs@gmial.com>
2025-05-20 19:53:58 -0400

    entity dto de users

--- src/auctions/entities/auction.entity.ts
+++ src/auctions/entities/auction.entity.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { text } from "stream/consumers";
-import { Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
     name:'auctions'
 })
-export class Auction {}
+export class Auction {
+    @PrimaryGeneratedColumn('uuid')
+    public id: string
+}
