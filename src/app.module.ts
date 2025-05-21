71e7e5aeb7196fa4b13a1da2d0076bb221adc032
omarenriquecs <omarenriquecs@gmial.com>
2025-05-20 21:53:15 -0400

    api property category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeOrmConfig from './config/typeorm';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { AuctionsModule } from './auctions/auctions.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -22,6 +25,9 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
       },
     }),
     UsersModule,
+    ProductsModule,
+    AuctionsModule,
+    CategoryModule
   ],
 })
 export class AppModule {}
--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class CreateCategoryDto {
+    @ApiProperty({
+        required: true,
+        description: 'Debe ingresar el nombre de la categoria',
+        example: 'Computadora'
+
+    })
     @IsNotEmpty()
     @IsString()
     categoryName: string
