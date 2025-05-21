ede71e7fab4ebab2e20e77d99097193623608e69
Your Name <bardanii7@gmail.com>
2025-05-20 22:24:01 -0300

    Cambios en database realizados

---
+++ 1747790530576-NuevaMigracion.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class NuevaMigracion1747790530576 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
